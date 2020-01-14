6a266d3a1d78a410fa34770f5156018ef7d033bb
yuuta66jp <yuuta66jp@yahoo.co.jp>
2020-01-14 16:37:46 +0900

    [Finish]5-4

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,10 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable
 
   has_many :post_images, dependent: :destroy
   has_many :post_comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   root 'post_images#index'
 
   resources :post_images, only: [:new, :create, :index, :show] do
+  	resource :favorites, only: [:create, :destroy]
     resource :post_comments, only: [:create, :destroy]
   end
 
