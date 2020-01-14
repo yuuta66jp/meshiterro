7245f434eb3996ed63f6327f428de2fc6c3583c9
yuuta66jp <yuuta66jp@yahoo.co.jp>
2020-01-14 14:08:55 +0900

    [Finish]5-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '6b00e5bb0bb7ef5753fa6e977b1f702eb67e9b22bc87565294ff496587bb46b4e963aa825ee7d50f3ec3ab3c7910ad6d6147ee7aa6288492e9e6104affe80251'
