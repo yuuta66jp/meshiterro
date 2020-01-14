f93dacb1ddf451096d158d1ba0397866c8629920
yuuta66jp <yuuta66jp@yahoo.co.jp>
2020-01-14 16:48:32 +0900

    [Start]5-5

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'post_images#index'
 
-  resources :post_images, only: [:new, :create, :index, :show] do
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
   	resource :favorites, only: [:create, :destroy]
     resource :post_comments, only: [:create, :destroy]
   end
