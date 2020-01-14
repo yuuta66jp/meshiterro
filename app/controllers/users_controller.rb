463a56f979910ea92108dcd7e5c377ebcff4add7
yuuta66jp <yuuta66jp@yahoo.co.jp>
2020-01-14 17:20:54 +0900

    [Start]5-6

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,4 +5,8 @@ class UsersController < ApplicationController
   	  @post_images = @user.post_images.page(params[:page]).reverse_order
   end
 
+  def edit
+  	  @user = User.find(params[:id])
+  end
+
 end
